25c8c5a09600eae4dbb18b033b8b3d8a47c3c74a
kapil97 <kragava1@binghamton.edu>
2020-09-20 23:18:14 -0400

    Binary Image Conversion

--- Assignment-1/main.cpp
+++ Assignment-1/main.cpp
@@ -60,7 +60,7 @@ void generateNegativeImage(cv::Mat img,cv::Mat mod)
 	
 }
 
-cv::Mat equalizeHistogram(cv::Mat &img){
+cv::Mat equalizeHistogram(cv::Mat &img, std::string win_name){
     // Total number of occurance of the number of each pixels at different levels from 0 - 256
     // Flattening our 2d matrix
     int flat_img[256] = {0};
@@ -88,6 +88,9 @@ cv::Mat equalizeHistogram(cv::Mat &img){
         normalize_img[i] = ((cumsum[i]-cumsum[0])*255)/(img.rows*img.cols-cumsum[0]);
         normalize_img[i] = static_cast<int>(normalize_img[i]);
     }
+	int scaled[256]={0};
+	std::copy(std::begin(normalize_img), std::end(normalize_img), std::begin(scaled));
+	dispHistogram(scaled,win_name);
 
     // convert 1d back into a 2d matrix
     cv::Mat result(img.rows, img.cols, CV_8U);
@@ -106,6 +109,81 @@ cv::Mat equalizeHistogram(cv::Mat &img){
     return result;
 }
 
+int getAverageIntensityValue(int hist[], int a, int b)													//Function to find average_intensity_value of histogram based on algorithm provided in the README file
+{
+	int sum_of_pixels=0;
+	sum_of_pixels = sum_of_pixels + hist[0];
+	int sum_product_of_pixels = (hist[0] * 0);
+
+	for (int i = a; i < b; i++)
+	{
+		sum_of_pixels = sum_of_pixels + hist[i];
+	}
+
+	for (int i = a; i < b; i++)
+	{
+		sum_product_of_pixels = sum_product_of_pixels + (hist[i] * i) ;
+	}
+
+	int average_intensity_value = sum_product_of_pixels / sum_of_pixels;														//average_intensity_value calculation code ends here
+	return average_intensity_value;
+}
+
+cv::Mat getBinaryImage(Mat img, int threshold)
+{
+	for (int i = 0; i < img.rows; i++)
+	{
+		for (int j = 0; j < img.cols; j++)
+		{
+			if (int(img.at<uchar>(i, j)) <= threshold)
+			{
+				img.at<uchar>(i, j) = 0;																						//If pixel intensity is greater than threshold
+			}
+			else
+			{
+				img.at<uchar>(i, j) = 255;																						//If pixel intensity is less than threshold
+			}
+		}
+	}
+
+	return img;
+}
+
+cv::Mat convertToBinary(Mat img){
+	int rows = img.rows;
+	int cols = img.cols;
+	int original_image_histogram_values[255];
+	for (int a = 0; a < 256; a++)																			//Setting histogram Y axis values to zero
+	{
+		original_image_histogram_values[a] = 0;
+	}
+
+	for (int i = 0; i < img.rows; i++)
+	{
+		for (int j = 0; j < img.cols; j++)
+		{
+			original_image_histogram_values[int(img.at<uchar>(i, j))]++;										//Generate histogram for original image
+		}
+	}
+
+	int old_threshold = 128;
+	int new_threshold = 128;
+	int delta_threshold = 1;
+	int m1, m2;
+	
+	do
+	{
+		old_threshold = new_threshold;
+		m1 = getAverageIntensityValue(original_image_histogram_values, 1, old_threshold);
+		m2 = getAverageIntensityValue(original_image_histogram_values, old_threshold + 1, 256);
+		new_threshold = (m1 + m2) / 2;
+		std::cout<<"Old Threshold "<<old_threshold<<"new threshold"<<new_threshold<<std::endl;
+		
+	}while ((old_threshold-new_threshold) > delta_threshold);
+	std::cout<<"Threshold"<<new_threshold<<std::endl;
+	img = getBinaryImage(img, new_threshold);
+	return img;
+}
 int main(int argc, char **argv) {
 	using namespace std;
 	using namespace cv;
@@ -119,7 +197,7 @@ int main(int argc, char **argv) {
 		original_img_hist[i]=0;
 		negative_img_hist[i]=0;
 	}
-
+	imshow("Original Image ",original_image);
 	generateHistogram(original_image ,original_img_hist);
 	dispHistogram(original_img_hist,"Original Image Histogram");
 
@@ -127,13 +205,23 @@ int main(int argc, char **argv) {
 	generateNegativeImage(original_image,modified_image);
 	generateHistogram(modified_image,negative_img_hist);
 	dispHistogram(negative_img_hist,"Negative Image Histogram");
-	Mat enhanced = equalizeHistogram(original_image);
+	Mat enhanced = equalizeHistogram(original_image,"After Equalization");
     imshow("Enhanced Image",enhanced);
 	Mat nyc_image=imread("NYC_width_4times.bmp",IMREAD_GRAYSCALE);
-	// generateHistogram(nyc_image,original_img_hist);
-	// generateHistogram(nyc_image,original_img_hist);
-	enhanced=equalizeHistogram(nyc_image);
-	imshow("Enhanced Image-New York",enhanced);
+	imshow("Before Equalization NYC",nyc_image);
+	enhanced=equalizeHistogram(nyc_image, "After Equalization of New York");
+	imshow("After Histogram Equalization",enhanced);
+
+	Mat shapes=imread("shapes2.1.bmp",IMREAD_GRAYSCALE);
+	imshow("shapes before binary conversion",shapes);
+	Mat res = convertToBinary(shapes);
+	imshow("shapes after Binary Conversion",res);
+	Mat dont_panic=imread("guide_8bits.bmp",IMREAD_GRAYSCALE);
+	imshow("Don't Panic Image before Binary Conversion",dont_panic);
+	res = convertToBinary(dont_panic);
+	imshow("Don't Panic Binary Image",res);
 	cv::waitKey(0);
 	destroyAllWindows();
+
+	return 0;
 }
